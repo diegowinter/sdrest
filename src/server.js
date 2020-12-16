225c43be90b1963b8f5419c45a62c926494cc081
Diego Winter <diego@diegowinter.dev>
2020-12-16 14:26:16 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -6,5 +6,5 @@ let port = process.env.PORT || 3000;
 const server = require('./route.js'); // imports the routing file
 
 server.listen(process.env.PORT || 3000 , () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at port ${port}/`);
 });
