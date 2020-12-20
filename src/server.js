4dd61b052a822f7409591f6edfeaca0de8280af3
Diego Winter <diego@diegowinter.dev>
2020-12-19 21:48:29 -0300

    Changed port

--- src/server.js
+++ src/server.js
@@ -1,10 +1,9 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = require('./route.js'); // imports the routing file
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(process.env.PORT || 3000 , () => {
+    console.log(`Server running at port ${port}/`);
 });
